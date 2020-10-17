d4d668a22d4c60e6a617c7dd4a524bdede134cdb
jsalazare <javi_9315@hotmail.com>
2020-10-17 15:25:57 -0700

    number-of-students-doing-homework-at-given-time solution

---
+++ src/main/java/com/javier/numberOfStudentsDoingHomeworkAtGivenTime/Solution.java
@@ -0,0 +1,14 @@
+package com.javier.numberOfStudentsDoingHomeworkAtGivenTime;
+
+public class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int counter = 0;
+        for (int i = 0; i < startTime.length; i++) {
+            if (startTime[i] <= queryTime && queryTime <= endTime[i]){
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+}

2b7221a9ef34a6573af49ff54be3490255a0e291
jsalazare <javi_9315@hotmail.com>
2020-10-26 18:31:28 -0700

    matrix-diagonal-sum solution

---
+++ src/main/java/com/javier/matrixDiagonaSum/Solution.java
@@ -0,0 +1,23 @@
+package com.javier.matrixDiagonaSum;
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int left = 0;
+        int right = mat.length - 1;
+        int sum = 0;
+
+        for (int i = 0; i < mat.length; i++) {
+
+            if (left == right){
+                sum += mat[i][left];
+            } else {
+                sum += mat[i][left] + mat[i][right];
+            }
+
+
+            left++;
+            right--;
+        }
+
+        return sum;
+    }
+}

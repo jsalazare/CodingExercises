468bb94efc9c5fb1b2ce38a7ba03f5bd6f8e3e08
jsalazare <javi_9315@hotmail.com>
2020-10-28 23:37:53 -0700

    find-numbers-with-even-number-of-digits solution

---
+++ src/main/java/com/javier/findNumbersWithEvenNumberOfDigits/Solution.java
@@ -0,0 +1,23 @@
+package com.javier.findNumbersWithEvenNumberOfDigits;
+
+class Solution {
+    public int findNumbers(int[] nums) {
+
+        int numEvens = 0;
+        for(int i = 0; i < nums.length; i++){
+
+            for (int numCeros = 10; numCeros >= 0; numCeros--){
+
+                if (nums[i] % Math.pow(10, numCeros) < nums[i]){
+                    if ((numCeros + 1) % 2 == 0) {
+
+                        numEvens++;
+                    }
+                    break;
+                }
+
+            }
+        }
+        return numEvens;
+    }
+}

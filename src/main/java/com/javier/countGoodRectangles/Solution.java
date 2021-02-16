258349b73ba4c0bbe2ca6359dc9a504657064bcc
jsalazare <javi_9315@hotmail.com>
2021-02-15 18:41:24 -0700

    number-of-rectangles-that-can-form-the-largest-square

---
+++ src/main/java/com/javier/countGoodRectangles/Solution.java
@@ -0,0 +1,23 @@
+package com.javier.countGoodRectangles;
+
+
+//https://leetcode.com/problems/number-of-rectangles-that-can-form-the-largest-square/
+class Solution {
+    public int countGoodRectangles(int[][] rectangles) {
+        int maxLen = Integer.MIN_VALUE;
+        int maxLenCounter = 0;
+        int rectangleMinSide = 0;
+        for (int i = 0; i < rectangles.length; i++) {
+            rectangleMinSide = Math.min(rectangles[i][0], rectangles[i][1]);
+
+            if (rectangleMinSide > maxLen){
+                maxLen = rectangleMinSide;
+                maxLenCounter = 1;
+            } else if (maxLen == rectangleMinSide){
+                maxLenCounter++;
+            }
+        }
+
+        return maxLenCounter;
+    }
+}
